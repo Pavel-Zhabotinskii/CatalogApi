8efa88c2f0cff9e51d887ebe3a77c5481feb95f5
Pavel <pavliszabotin@gmail.com>
2023-10-06 16:35:45 +0200

    first commit

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new() 
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTime.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTime.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze", Price = 18, CreatedDate = DateTime.Now }
+        };
+
+        public IEnumerable<Item> GetItems() => items;
+
+        public Item GetItem(Guid id) => items.Where(item => item.Id == id).SingleOrDefault();
+    }
+}
