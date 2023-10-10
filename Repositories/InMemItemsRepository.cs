4b5770e94a42bbbd310021cb04fa94d203a37633
Pavel <pavliszabotin@gmail.com>
2023-10-10 18:02:16 +0200

    added honeypots Post,Put and Delete to the controller

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -14,5 +14,15 @@ namespace Catalog.Repositories
         public IEnumerable<Item> GetItems() => items;
 
         public Item GetItem(Guid id) => items.Where(item => item.Id == id).SingleOrDefault();
+
+        public void CreateItem(Item item) => items.Add(item);
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            items[index] = item;
+        }
+
+        public void DeleteItem(Guid id) => items.RemoveAt(items.FindIndex(existingItem => existingItem.Id == id));
     }
 }
